1567e81212fc08f017ab428a4c2ef3d28df2e8a6
blaiddyd <bukulmezekin@gmail.com>
2024-09-29 16:02:01 +0800

    Initial files and route setup!

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import { Link } from "@chakra-ui/next-js";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>Welcome to your cute anime recommender~!</h1>
+      <Link href="/recommendations">
+        Get started!
+      </Link>
+    </div>
+  );
+}
---
+++ src/app/recommendations/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Recommendations() {
+  return (
+    <div className={styles.page}>
+      <h1>Recommendations</h1>
+      
+    </div>
+  );
+}
